14cd34dab981ae57403212898c4bb15743ab7f04
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-04 19:32:17 +0100

    File for validation functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const validation = require("./lib/validation");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -14,6 +15,16 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 ​
+async function teamGenerator() {
+    try {
+        const answers = await inquirer.prompt ({
+
+        })
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
