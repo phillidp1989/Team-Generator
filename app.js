e270bd51b2430de74121c9453b4e7b840f02d9a8
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-06 20:54:20 +0100

    Validation to prevent overwritten html

--- app.js
+++ app.js
@@ -7,25 +7,31 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
+
+let fileName = "team"
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+let outputPath = path.join(OUTPUT_DIR, `${fileName}.html`);
+
+const render = require("./lib/htmlRenderer");
+
 // Creating Promise-based version of fs modules
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 // Define global variable to hold the role of the member being added
 
 let memberRole = null;
-let team = [];
+let teamName;
 
-// Object used for asking the user the appropriate question based on the role selected
+// Empty arrays which new instantiated employee objects will be pushed into. Multiple arrays have been used to ensure each role type are grouped when the html is rendered
+
+let team = [];
+let managers = [];
+let engineers = [];
+let interns = [];
+
+// Object used for asking the appropriate question based on the role selected
 
 const specialQuestion = {
     "Manager": "office number",
@@ -33,10 +39,9 @@ const specialQuestion = {
     "Intern": "school name"
 };
 
+// Function to ask user which team member type they would like to add next or to complete the team generator
 
-// Function to ask user which team member type they would like to add next
-
-const roleQuestion = async () => {
+const roleQuestion = async teamName => {
     const { memberType } = await inquirer.prompt([
         {
             type: "list",
@@ -46,27 +51,55 @@ const roleQuestion = async () => {
 
         },
     ]);
+
+    // If statement which stops the question loop, renders the objects, creates the HTML file and console logs a success message
+
     if (memberType === "I have finished adding members to my team") {
-        
-        const html = render(team);
-        
+
+        // Concatenate arrays so that employee types are grouped together
+
+        team = [...managers, ...engineers, ...interns];
+
+        // Call the render function and pass in employee objects and team name
+
+        const html = render(team, teamName);
+
+        if (fs.existsSync(outputPath)) {
+            const { updateFileName } = await inquirer.prompt({
+                type: "input",
+                message: `A file named ${fileName}.html already exists within the 'output' directory. Please provide an alternative filename so that the original document is not overwritten. Please do not include the file extension in your response`,
+                name: "updateFileName",
+                validate: validation.fileValidation
+            })
+
+            // Reasignment of variables based on user input
+
+            fileName = updateFileName;
+            outputPath = path.join(OUTPUT_DIR, `${fileName}.html`);
+        }
+
         await writeFileAsync(outputPath, html);
 
-        console.log("\nThank you for using the team generator CLI. Your team summary is available in this folder");
+        console.log("\nThank you for using the team generator CLI. Your team summary is available in the 'output' folder\n");
         return;
     }
-    memberData(memberType);
-}
+
+    // If the user selects another role to add to the team, the memberData function will be called with the memberType and teamName passed in as arguments
+
+    memberData(memberType, teamName);
+};
+
+// Function to collect team member data using inquirer
 
 
-const memberData = async memberRole => {
+const memberData = async (memberRole, teamName) => {
 
     let special = specialQuestion[memberRole];
 
     const { memberName } = await inquirer.prompt([
         {
             type: "inupt",
-            message: `What is the name of the ${memberRole} you are adding to your team?`,
+            message: `What is the name of the ${memberRole} you are adding to ${teamName}?`,
             name: "memberName",
             validate: validation.nameValidation
         }
@@ -95,59 +128,61 @@ const memberData = async memberRole => {
             validate: validation.nameValidation
         },
 
-    ])
+    ]);
 
     // Switch statement to instantiate relevant object (manager, engineer or intern) based on role type selected by user
 
     switch (memberRole) {
         case "Manager":
             employee = new Manager(memberName.trim(), answers.memberID.trim(), answers.memberEmail.trim(), answers.memberSpecial.trim());
-            team.push(employee);
-            console.log(team);
+            managers.push(employee);
             break;
         case "Engineer":
             employee = new Engineer(memberName.trim(), answers.memberID.trim(), answers.memberEmail.trim(), answers.memberSpecial.trim());
-            team.push(employee);
-            console.log(team);
+            engineers.push(employee);
             break;
         case "Intern":
             employee = new Intern(memberName.trim(), answers.memberID.trim(), answers.memberEmail.trim(), answers.memberSpecial.trim());
-            team.push(employee);
-            console.log(team);
+            interns.push(employee);
             break;
         default:
-            console.log("Something went wrong");
+            console.log("Error! Something went wrong");
             break;
     }
-    roleQuestion();
+
+    // Once the employee object has been created, the roleQuestion function is called to ask the user if they wish to add a new member or complete the process
+
+    roleQuestion(teamName);
 };
 
-// Function to welcome user and set type to manager initially
+// Function to welcome user and set role to manager
 
-const welcomeManager = () => {
+const welcomeManager = async () => {
     if (memberRole === null) {
-        console.log("Welcome to the Software Engineering Team Generator. \nPlease answer the following questions in order to build your team summary\n");
+        console.log("\n\nWelcome to the Software Engineering Team Generator. \nPlease answer the following questions in order to build your team summary\n");
     }
+
+    const { nameTeam } = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of your team?",
+            name: "nameTeam",
+            validate: validation.nameValidation
+
+        },
+    ]);
 
     // Set the type variable to manager as the user story suggests that the manager will be using this CLI team generator
 
+    teamName = nameTeam;
     memberRole = "Manager";
-    memberData(memberRole)
+    memberData(memberRole, teamName);
 
-}
+};
+
+// Call the welcomeManager function to inititate the CLI
 
 welcomeManager();
 
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
 
